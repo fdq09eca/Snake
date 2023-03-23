267efb3cc68920412baed4ba962bcb2e5e2168dd
ChrisLam <fdq09eca@gmail.com>
2023-03-23 04:19:09 +0000

    1 - 13 bitmap

--- Snake/Resource.h
+++ Snake/Resource.h
@@ -1,30 +1,40 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by Snake.rc
-
-#define IDS_APP_TITLE			103
-
-#define IDR_MAINFRAME			128
-#define IDD_SNAKE_DIALOG	102
-#define IDD_ABOUTBOX			103
-#define IDM_ABOUT				104
-#define IDM_EXIT				105
-#define IDI_SNAKE			107
-#define IDI_SMALL				108
-#define IDC_SNAKE			109
-#define IDC_MYICON				2
-#ifndef IDC_STATIC
-#define IDC_STATIC				-1
-#endif
-// Next default values for new objects
 //
+#define IDC_MYICON                      2
+#define IDD_SNAKE_DIALOG                102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_SNAKE                       107
+#define IDI_SMALL                       108
+#define IDC_SNAKE                       109
+#define IDR_MAINFRAME                   128
+#define IDB_BITMAP1                     129
+#define IDB_BITMAP2                     130
+#define IDB_BITMAP3                     131
+#define IDB_BITMAP4                     132
+#define IDB_BITMAP5                     133
+#define IDB_BITMAP6                     134
+#define IDB_BITMAP7                     135
+#define IDB_BITMAP8                     136
+#define IDB_BITMAP9                     137
+#define IDB_BITMAP10                    138
+#define IDB_BITMAP11                    139
+#define IDB_BITMAP12                    140
+#define IDB_BITMAP13                    141
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NO_MFC					130
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_COMMAND_VALUE		32771
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		110
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        142
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
